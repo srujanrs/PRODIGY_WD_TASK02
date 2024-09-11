aef1e0d901383ff2fdd6355a5d8f8f946a0eed4a
srujanrs <srujanrs30@gmail.com>
2024-09-11 10:56:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timeRef = document.querySelector(".timer-display");
+let lapContainer = document.querySelector(".laps");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+    if (int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+    clearInterval(int);
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timeRef.innerHTML = "00 : 00 : 00 : 000 ";
+    lapContainer.innerHTML = ""; // Clear laps
+});
+
+document.getElementById("lap-timer").addEventListener("click", () => {
+    let lapTime = timeRef.innerHTML;
+    let lapElement = document.createElement("div");
+    lapElement.className = "lap";
+    lapElement.innerText = lapTime;
+    lapContainer.appendChild(lapElement);
+});
+
+function displayTimer() {
+    milliseconds += 10;
+    if (milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = 
+        milliseconds < 10
+        ? "00" + milliseconds
+        : milliseconds < 100
+        ? "0" + milliseconds
+        : milliseconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
